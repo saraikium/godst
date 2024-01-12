5272a1f116ad089a2b41436b6a1c360e0fe7aac8
saraikium <adnank122@gmail.com>
2024-01-13 02:00:25 +0500

    Feat(Queues): Add tests for Queues

---
+++ queues/arrayqueue_test.go
@@ -0,0 +1,42 @@
+package queues
+
+import (
+	"testing"
+)
+
+func TestEnqueue(t *testing.T) {
+
+	q := New[int]()
+
+	_, err := q.Dequeue()
+
+	if err == nil {
+		t.Errorf("Empty queue should throw an error on Dequeue.")
+	}
+
+	q.Enqueue(5)
+	q.Enqueue(6)
+
+	if q.Size() != 2 {
+		t.Errorf("Size should be 2. Got=%v", q.Size())
+	}
+
+	v, err := q.Dequeue()
+
+	if err != nil {
+		t.Errorf("Value should be 5, got=%v", v)
+	}
+
+	v, err = q.Dequeue()
+
+	if err != nil {
+		t.Errorf("Value should be 6, got=%v", v)
+	}
+
+	_, err = q.Dequeue()
+
+	if err == nil {
+		t.Errorf("Empty queue should throw an error on Dequeue.")
+	}
+
+}
