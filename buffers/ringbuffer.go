f71ede24b7c5807ccc452d71c30836b757b82b79
saraikium <adnank122@gmail.com>
2024-01-13 09:31:02 +0500

    Feat(RingBuffer): Add Ring Buffer data structure

--- buffers/ringbuffer.go
+++ buffers/ringbuffer.go
@@ -1,6 +1,7 @@
 package buffers
 
 import (
+	"fmt"
 	"sync"
 )
 
@@ -19,4 +20,39 @@ func New[T any](size int) *RingBuffer[T] {
 	}
 
 	return r
+}
+
+func (rb *RingBuffer[T]) Enqueue(v T) error {
+	rb.mutex.Lock()
+	defer rb.mutex.Unlock()
+
+	if (rb.tail+1)%rb.size == rb.head {
+		return fmt.Errorf("RingBuffer is full.")
+	}
+
+	rb.data[rb.tail] = v
+	rb.tail = (rb.tail + 1) % rb.size
+	return nil
+}
+
+func (rb *RingBuffer[T]) Deque() (T, error) {
+	rb.mutex.Lock()
+	defer rb.mutex.Unlock()
+
+	var zeroVal T
+	if rb.tail == rb.head {
+		return zeroVal, fmt.Errorf("Empty Buffer")
+	}
+	data := rb.data[rb.head]
+	rb.head = (rb.head + 1) % rb.size
+
+	return data, nil
+}
+
+func (r *RingBuffer[T]) IsEmpty() bool {
+	return r.head == r.tail
+}
+
+func (r *RingBuffer[T]) IsFull() bool {
+	return (r.tail+1)%r.size == r.head
 }
